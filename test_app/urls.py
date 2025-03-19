4b74ab8277faf91f0ea48d88248fda2861c21bf7
MarvinMathewIgnatious05 <imarvinmathew@gmail.com>
2025-03-19 14:40:16 +0530

    practical test for multiple subjects

--- test_app/urls.py
+++ test_app/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
-from .views import view_question, start_mock_test, mock_test_result, start_practical_test, practical_test_result
+from .views import view_question, start_mock_test, mock_test_result, start_practical_test, practical_test_result, subject_list, select_subject, chapter_list
 
 urlpatterns = [
 
     path('view/',view_question, name="view_question"),
     path("start/", start_mock_test, name="start_mock_test"),
     path("result/<int:test_id>/", mock_test_result, name="mock_test_result"),
-    path('pt/start/<int:subject_id>/', start_practical_test, name="practical_test"),
-    path('submit/<int:submission_id>/', practical_test_result, name='practical_test_result'),
+    path('pt/start/<int:chapter_id>/', start_practical_test, name="practical_test"),
+    path('submit/<int:test_id>/', practical_test_result, name='practical_test_result'),
+    path("subjects/", subject_list, name="subject_list"),
+    path("subjects/<int:subject_id>/", select_subject, name="select_subject"),
+    path("subjects/<int:subject_id>/chapters/", chapter_list, name="chapter_list"),
 
 ]
 
@@ -17,11 +20,5 @@ urlpatterns = [
 
 
 
-
-# from django.urls import path
-# from .views import start_practical_test, practical_test_result
-#
-# urlpatterns = [
-#     path("practical-test/<int:practical_test_id>/", start_practical_test, name="start_practical_test"),
-#     path("practical-test/<int:practical_test_id>/submit/", practical_test_result, name="practical_test_result"),
+#     path("subjects/<int:subject_id>/chapters/", chapter_list, name="chapter_list"),  # Show chapters of the selected subject
 # ]
--- test_app/views.py
+++ test_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect,  get_object_or_404, HttpResponse
-from test_app.models import Subject, Question, MockTest, MockTestSubmission, PracticalTest, PracticalTestSubmission
+from test_app.models import Subject, Chapter, Question, MockTest, MockTestSubmission, PracticalTest, PracticalTestSubmission
 from django.contrib.auth.decorators import login_required
 from .forms import MockTestSubmissionForm
 from django.utils.timezone import now
@@ -39,6 +39,7 @@ def start_mock_test(request):
             is_correct = selected_option == question.correct_option if selected_option else False
 
 
+
             MockTestSubmission.objects.create(
                 mock_test=mock_test,
                 student=student,
@@ -65,11 +66,12 @@ def mock_test_result(request, test_id):
     mock_test = get_object_or_404(MockTest, id=test_id, student=request.user)
     print("mock_test",mock_test)
     submissions = MockTestSubmission.objects.filter(mock_test=mock_test)
-    print("sub",submissions)
+    print("sub", submissions)
 
     total_questions = submissions.count()
     correct_answers = submissions.filter(is_correct=True).count()
     score = (correct_answers / total_questions) * 100 if total_questions else 0
+    print("score-mocktest", score)
 
     return render(request, "mock_test_result.html", {
         "mock_test": mock_test,
@@ -81,75 +83,115 @@ def mock_test_result(request, test_id):
 
 
 
+def subject_list(request):
+    subjects = Subject.objects.filter(is_active=True)
+    print("all/////////", subjects)
+    return render(request, "subject_list.html", {"subjects": subjects})
+
+
+
+
+def select_subject(request, subject_id):
+    return redirect("chapter_list", subject_id=subject_id)
+
+
+def chapter_list(request, subject_id):
+    subject = get_object_or_404(Subject, id=subject_id)
+    print("sub",subject)
+    chapters = Chapter.objects.filter(subject=subject, is_active=True)
+    print("////////////////////////////////",chapters)
+    return render(request, "chapter_list.html", {"subject": subject, "chapters": chapters})
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 @login_required(login_url="/auth/login/")
-def start_practical_test(request, subject_id):
+def start_practical_test(request, chapter_id):
 
-
+    chapter = get_object_or_404(Chapter, id=chapter_id, is_active=True)
+    print("chapter",chapter)
+    subject = chapter.subject
     student = request.user
+    print("student",student)
+
+
     practical_test, created = PracticalTest.objects.get_or_create(
         student=student,
-        subject_id=subject_id,
+        subject=subject,
+        organization=subject.organization,
         completed=False
     )
 
-    questions = Question.objects.filter(chapter__subject_id=subject_id).order_by("question_no")
+    questions = Question.objects.filter(chapter=chapter).order_by("question_no")
 
     if request.method == "POST":
+        answers = {}
+        total_questions = questions.count()
         total_attended = 0
         correct_answers = 0
-        submitted_answers = {}
 
         for question in questions:
-            selected_option = request.POST.get(f"question_{question.id}")
-            print("selected",selected_option)
-            is_correct = selected_option == question.correct_option if selected_option else False
+            selected_option = request.POST.get(f"question_{question.id}", "N")
+            print("selected", selected_option)
+            is_correct = selected_option == question.correct_option
 
-            if selected_option:
+            answers[str(question.id)] = selected_option
+            if selected_option != "N":
                 total_attended += 1
-                print("total/////////////////////",total_attended)
-                submitted_answers[question.id] = selected_option
-                if is_correct:
-                    correct_answers += 1
+            if is_correct:
+                correct_answers += 1
 
-        score = (correct_answers / questions.count()) * 100 if questions.exists() else 0
-        print("//////////////////////",score)
+        score = (correct_answers / total_questions) * 100 if total_questions else 0
+        print("score",score)
 
-        submission = PracticalTestSubmission.objects.create(
+        PracticalTestSubmission.objects.create(
             practical_test=practical_test,
             student=student,
-            subject=practical_test.subject,
-            answers=str(submitted_answers),
-            total_attended=total_attended,
-            unattended=questions.count() - total_attended,
+            subject=subject,
+            answers=str(answers),
             score=score,
-            test_average_time=timedelta(seconds=120),
-            completed=True,
-            submitted_at=now(),
+            unattended=total_questions - total_attended,
+            total_attended=total_attended,
+            completed=True
         )
 
         practical_test.completed = True
         practical_test.save()
-        print("pppppppppppppppppppp",practical_test)
 
-        return redirect("practical_test_result", submission_id=submission.id)
+        messages.success(request, "Practical test submitted successfully.")
+        return redirect("practical_test_result", test_id=practical_test.id)
 
     return render(request, "practical_test.html", {
         "practical_test": practical_test,
+        "subject": subject,
+        "chapter": chapter,
         "questions": questions,
     })
 
 
 
-
-
 @login_required
-def practical_test_result(request, submission_id):
+def practical_test_result(request, test_id):
 
-    submission = get_object_or_404(PracticalTestSubmission, id=submission_id, student=request.user)
-    # print("ssssssssssssssss",submission)
+    practical_test = get_object_or_404(PracticalTest, id=test_id, student=request.user)
+    submission = PracticalTestSubmission.objects.filter(practical_test=practical_test).first()
+
+    # if not submission:
+    #     messages.error(request, "No submission found for this test.")
+    #     return redirect("subject_list")
 
     return render(request, "pratical_test_result.html", {
+        "practical_test": practical_test,
         "submission": submission,
     })
