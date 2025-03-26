b21c60eda7de47ca8ccdca60d4550f4925c73462
MarvinMathewIgnatious05 <imarvinmathew@gmail.com>
2025-03-26 14:29:08 +0530

    Added user profile view and edit functionality

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import user_registration, user_login, user_logout
+from .views import user_registration, user_login, user_logout, user_profile_view, user_profile_edit
 
 app_name = "authentication"
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path("register/", user_registration, name="user_registration"),
     path("login/", user_login, name="user_login"),
     path("logout/", user_logout, name="user_logout"),
-
+    path("user_view/", user_profile_view, name="user_view"),
+    path("user_edit/", user_profile_edit, name="user_edit"),
 ]
